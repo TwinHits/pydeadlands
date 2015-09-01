fa46c763fb54c4f1706d73cb6334ed0ce4e71bf2
twinhits <twinhits@gmail.com>
2015-09-01 12:49:09 -0400

    Completed Encounter class that generates tracks and iterates Turn instances and Round instances.

--- encounters.py
+++ encounters.py
@@ -11,41 +11,89 @@ class Encounter():
         Possible Keywords:
             surprise=True 
     """
-
-    def __init__(players, marshals, **kwargs):
+    def __init__(self, players, marshal, **kwargs):
         self.over = False
         self.players = players
-        self.marshals = marshals
+        self.marshal = marshal
         self.players_deck = Deck()
-        self.marshals_deck = Deck()
+        self.marshal_deck = Deck()
 
         self.surprise = False
 
         for k in kwargs:
             if k == surprise:
                 self.surprise = surprise
-
         
-    def begin():
+    def begin(self):
         """Calling this method begins the ecounter. When this method returns,
         the encounter is over."""
-        while(!self.over):
-            _round = Round()
-            while(!_round.over):
+        while(self.over == False):
+            _round = Round(self)
+            while(_round.over == False):
                 #The characters take action 
                 _round._next()
-                
+            #check if encounter is over
+            self.end()
+            self.over = True
 
-    class Round():
-        def __init__():
-            self.over == False
-            #Do the card drawing and turn organizing
-        
-        def _next():
-            """Calling this method gets the next character in line to start
-            combat."""
-            #return the next character to go
+    def end(self):
+        #destructor actions
+        pass
 
+class Round():
+    """A round is defined by each character drawing cards and taking
+    actions."""
+    def __init__(self, encounter):
+        self.over = False
+        self.encounter = encounter
+        self.round_cards = {}
+        self.turns = []
 
-if __name__ == "__main__":
-    pass
+        self.__roll_quickness(self.encounter.players, self.encounter.players_deck)
+        self.__roll_quickness(self.encounter.marshal, self.encounter.marshal_deck)
+
+        for char in self.encounter.players:
+            for card in self.round_cards[char]:
+                self.turns.append(Turn(char, card))
+                   
+        for char in self.encounter.marshal:
+            for card in self.round_cards[char]:
+                self.turns.append(Turn(char, card))
+
+        self.turns.sort(reverse=True, key=lambda x: x.value)
+
+        #Test in cout
+        for t in self.turns:
+            print(t.value, t.character.name)
+
+    def __roll_quickness(self, characters, deck):
+        """Takes a list of characters and a deck and returns the cards for those
+        characters as a dictionary"""
+        for c in characters:
+            roll = c.quickness.roll() 
+
+            if roll == 0:
+                turns = 0
+            else:
+                turns = int(roll/5 + 1)
+
+            cards = [deck.draw() for i in range(0, turns)]
+            
+            #check for black joker
+
+            self.round_cards[c] = cards 
+
+    def _next(self):
+        """Calling this method gets the next character in line to start
+        combat."""
+        #return the next character to go
+        #if there are no more characters to go
+        self.over = True
+
+class Turn():
+    """A single turn in a round. Takes a character and a card. Surfaces the
+    card value for easier ordering"""
+    def __init__(self, character, card):
+        self.character = character
+        self.card = card
+        self.value = self.card.value
